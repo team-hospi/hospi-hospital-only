350d99f382ffadc9c7fea29d0c7cf65ba6317928
Bbangjun <gkvn1@naver.com>
2021-03-21 22:45:14 +0900

    문의 수정중

--- hospi-hospital-only/Inquiry.cs
+++ hospi-hospital-only/Inquiry.cs
@@ -24,10 +24,23 @@ namespace hospi_hospital_only
         public long timestamp { get; set; }
         [FirestoreProperty]
         public string title { get; set; }
+        [FirestoreProperty]
+        public string id { get; set; }
 
         FirestoreDb fs;
 
         public static List<Inquiry> list = new List<Inquiry>();
+
+        private static string FBdir = "hospi-edcf9-firebase-adminsdk-e07jk-ddc733ff42.json";
+
+        public void FireConnect()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + @FBdir;
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", path);
+
+            fs = FirestoreDb.Create("hospi-edcf9");
+        }
+
         //문의 받아오기
         public async void Inquiry_Open(string hospitalID)
         {
@@ -36,10 +49,21 @@ namespace hospi_hospital_only
 
             foreach (DocumentSnapshot docsnap in snap.Documents)
             {
-                Dictionary<string, object> inquiry = docsnap.ToDictionary();
-                foreach(KeyValuePair<string, object> pair in inquiry)
+                Dictionary<string, object> inquiryDictionary = docsnap.ToDictionary();
+                foreach(KeyValuePair<string, object> pair in inquiryDictionary)
                 {
-
+                    DocumentReference docRef = fs.Collection("inquiry_list").Document(pair.Key);
+                    DocumentSnapshot snapshot = await docRef.GetSnapshotAsync();
+                    if(snapshot.Exists)
+                    {
+                        Inquiry inquiry = snapshot.ConvertTo<Inquiry>();
+                        list.Add(inquiry);
+                        
+                    }
+                    else
+                    {
+                        
+                    }
                 }
 
             }
