6a2dcb061602213991670fd54ef2874ca79b2de2
Bbangjun <gkvn1@naver.com>
2021-03-21 22:45:14 +0900

    문의 수정중

--- hospi-hospital-only/InquiryCheck.Designer.cs
+++ hospi-hospital-only/InquiryCheck.Designer.cs
@@ -33,6 +33,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -71,12 +75,46 @@
             // 
             // listView1
             // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(12, 24);
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(26, 12);
+            this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(412, 387);
-            this.listView1.TabIndex = 5;
+            this.listView1.Size = new System.Drawing.Size(394, 399);
+            this.listView1.TabIndex = 11;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "⁯회원ID";
+            this.columnHeader1.Width = 70;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "날짜";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "제목";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "답변여부";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 70;
             // 
             // InquiryCheck
             // 
@@ -102,5 +140,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
     }
 }
--- hospi-hospital-only/InquiryCheck.cs
+++ hospi-hospital-only/InquiryCheck.cs
@@ -15,8 +15,10 @@ namespace hospi_hospital_only
     public partial class InquiryCheck : Form
     {
         DBClass dbc = new DBClass();
+        Inquiry inquiry = new Inquiry();
         FirestoreDb fs;
         string hospitalID;
+        string Check;
 
         public string HospitalID
         {
@@ -30,17 +32,35 @@ namespace hospi_hospital_only
 
         private void InquiryCheck_Load(object sender, EventArgs e)
         {
-            dbc.FireConnect();
+            inquiry.FireConnect();
 
             dbc.Delay(200);
+            inquiry.Inquiry_Open(hospitalID);
 
+            dbc.Delay(400);
 
-
+            for (int i = 0; i < Inquiry.list.Count; i++)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = Inquiry.list[i].id;
+                item.SubItems.Add(Inquiry.list[i].timestamp.ToString());
+                item.SubItems.Add(Inquiry.list[i].title);
+                if (Inquiry.list[i].checkedAnswer == true)
+                {
+                    Check = "O";
+                }
+                else
+                {
+                    Check = "X";
+                }
+                item.SubItems.Add(Check);
+                listView1.Items.Add(item);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            dbc.FireConnect();
+
         }
 
     }
