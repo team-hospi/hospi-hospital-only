546c87e2d233549eed5c68d63eb19e233ca75eee
wpdlem2 <wpdlem2@nate.com>
2021-04-12 12:52:32 +0900

    4/12

--- hospi-hospital-only/PDFTest.cs
+++ hospi-hospital-only/PDFTest.cs
@@ -7,15 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using iTextSharp.text;
-using iTextSharp.text.pdf;
-
 
 namespace hospi_hospital_only
 {
     public partial class PDFTest : Form
     {
+        public static Bitmap bmpScreenCapture;
+
         public PDFTest()
         {
             InitializeComponent();
@@ -23,9 +21,13 @@ namespace hospi_hospital_only
 
         private void button1_Click(object sender, EventArgs e)
         {
-            FileStream fs = new FileStream("Hello.pdf", FileMode.Create, FileAccess.Write, FileShare.None);
-            Document document = new Document(PageSize.A4);
-            PdfWriter writer = PdfWriter.GetInstance(document, fs);
+            bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (Graphics g = Graphics.FromImage(bmpScreenCapture))
+            {
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, 
+                                              Screen.PrimaryScreen.Bounds.Y, 0, 0, bmpScreenCapture.Size, 
+                                              CopyPixelOperation.SourceCopy);
+            }
 
         }
     }
