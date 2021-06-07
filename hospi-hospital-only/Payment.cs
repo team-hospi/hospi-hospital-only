78a7e7fc6a95a6bb99141a2c16c3dfd8bfe94852
Park_Byeong_Jun <gkvn1@naver.com>
2021-06-07 15:12:50 +0900

    에러 수정

--- hospi-hospital-only/Payment.cs
+++ hospi-hospital-only/Payment.cs
@@ -183,7 +183,7 @@ namespace hospi_hospital_only
                 
                 dbc.FindOpinion(Convert.ToInt32(patientID), receptionDate, receptionTime);
                 dbc.PrescriptionTable = dbc.DS.Tables["Prescription"];
-                if(dbc.PrescriptionTable.Rows.Count != 0)
+                for (int i = 0; i < dbc.PrescriptionTable.Rows.Count; i++)
                 {
                     Medicine.Add(dbc.PrescriptionTable.Rows[i][1].ToString());
                 }
